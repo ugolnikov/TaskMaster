806e8f3c1ddb9eb6597174d1115d2cca2f8d99ff
ugolnikov <42218732+ugolnikov@users.noreply.github.com>
2024-01-24 12:21:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ import tkinter as tk
 from tkinter import messagebox, simpledialog
 import sqlite3
 from tkinter import filedialog
+
+root = tk.Tk()
+root.title("Главное меню")
+root.geometry("200x300")
+
+
+
+root.mainloop()
