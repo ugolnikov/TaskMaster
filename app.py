f16db470288d99fe9440ddb05d4a9e8a7f95ac00
ugolnikov <42218732+ugolnikov@users.noreply.github.com>
2024-01-24 12:34:29 +0300

    goto

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ import sqlite3
 from tkinter import filedialog
 from sqlalchemy import column
 
-def goto_auth():
-    import auth.py
+def goto(module_name):
+    root.destroy()
+    import importlib
+    mod = importlib.import_module(module_name)
+    
 
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.grid()
 root.title("Главное меню")
 root.geometry("200x300")
-tk.Button(frame, text="Авторизация", command=root.destroy).grid(column=1,row=0)
+tk.Button(frame, text="Авторизация", command=lambda: goto("auth")).grid(column=1,row=0)
 tk.Button(frame, text="Регистрация", command=root.destroy).grid(column=1,row=1)
 tk.Button(frame, text="Выход", command=root.destroy).grid(column=1,row=2)
 
