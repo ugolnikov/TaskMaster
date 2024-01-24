f8b013be728c404073b6387e5c2b560b63c443fd
ugolnikov <42218732+ugolnikov@users.noreply.github.com>
2024-01-24 12:49:46 +0300

    123

--- app.py
+++ app.py
@@ -14,11 +14,12 @@ root = tk.Tk()
 root.iconbitmap("1.ico")
 frame = tk.Frame(root)
 frame.pack()
-root.title("Главное меню")
-root.geometry("200x300")
+root.title("Меню")
+root.geometry("220x300")
+tk.Label(frame, text="TaskMaster", fg="black", background="white").pack(side="top",fill="x",pady=10)
 tk.Button(frame, text="Авторизация", command=lambda: goto("auth")).pack(side="top",fill="x", pady=30)
-tk.Button(frame, text="Регистрация", command=lambda: goto("register")).pack(side="top",fill="x", pady=30)
-tk.Button(frame, text="Выход", command=root.destroy).pack(side="top",fill="x", pady=30)
+tk.Button(frame, text="Регистрация", command=lambda: goto("register")).pack(side="top",fill="x", pady=20)
+tk.Button(frame, text="Выход", command=root.destroy).pack(side="top",fill="x", pady=20)
 
 
 
