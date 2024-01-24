b1f179facda51a91ee106860663dca1aa94f4d4e
ugolnikov <42218732+ugolnikov@users.noreply.github.com>
2024-01-24 12:28:23 +0300

    pasha huy

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import tkinter as tk
 from tkinter import messagebox, simpledialog
 import sqlite3
 from tkinter import filedialog
-
 from sqlalchemy import column
+
+def goto_auth():
+    import auth.py
 
 root = tk.Tk()
 frame = tk.Frame(root)
@@ -11,6 +13,8 @@ frame.grid()
 root.title("Главное меню")
 root.geometry("200x300")
 tk.Button(frame, text="Авторизация", command=root.destroy).grid(column=1,row=0)
+tk.Button(frame, text="Регистрация", command=root.destroy).grid(column=1,row=1)
+tk.Button(frame, text="Выход", command=root.destroy).grid(column=1,row=2)
 
 
 
