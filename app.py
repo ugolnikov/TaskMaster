c34e7f57ae22b1c882c275252784f5ce0ecff12f
ugolnikov <42218732+ugolnikov@users.noreply.github.com>
2024-01-24 12:24:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from tkinter import messagebox, simpledialog
 import sqlite3
 from tkinter import filedialog
 
+from sqlalchemy import column
+
 root = tk.Tk()
+frame = tk.Frame(root)
+frame.grid()
 root.title("Главное меню")
 root.geometry("200x300")
+tk.Button(frame, text="Авторизация", command=root.destroy).grid(column=1,row=0)
+
 
 
 
